16d053ee8301cbe2c95dc83f4d442406bbb0fc05
leon5130c <50125595+leon5130c@users.noreply.github.com>
2019-06-14 21:46:24 -0300

    Add files via upload

---
+++ Quem samos.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="css/01-Style.css">
+</head>
+
+<body class="style">
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <title>Turismos</title>
+  <link rel="stylesheet" type="text/css" href="style.css" style="">
+  <div class="py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 my-3 px-4">
+          <h4>logotipo</h4>
+          <p class="mb-0">logomaca</p>
+        </div>
+        <div class="col-md-6 border-left my-3 px-4">
+          <div class="row">
+            <div class="col-md-12 text-center" style=""> <i class="d-block fa fa-stop-circle mb-3 text-muted fa-3x"></i>
+              <p> <a href="https://goo.gl/maps/AUq7b9W7yYJ2" target="_blank"> Fake street, 100 <br>NYC - 20179, USA</a> </p>
+              <p> <a href="tel:+246 - 542 550 5462">+1 - 256 845 87 86</a> </p>
+              <p class="mb-0"> <a href="mailto:info@pingendo.com">info@pingendo.com</a> </p>
+              <div class="row">
+                <div class="col-md-12 d-flex align-items-center justify-content-center my-3"> <a href="#">
+                    <i class="d-block fa fa-facebook-official text-muted fa-lg mr-2"></i>
+                  </a> <a href="#">
+                    <i class="d-block fa fa-instagram text-muted fa-lg mx-2"></i>
+                  </a> <a href="#">
+                    <i class="d-block fa fa-google-plus-official text-muted fa-lg mx-2"></i>
+                  </a> <a href="#">
+                    <i class="d-block fa fa-pinterest-p text-muted fa-lg mx-2"></i>
+                  </a> <a href="#">
+                    <i class="d-block fa fa-reddit text-muted fa-lg mx-2"></i>
+                  </a> <a href="#">
+                    <i class="d-block fa fa-twitter text-muted fa-lg ml-2"></i>
+                  </a> </div>
+                <div class="col-md-12 text-center">
+                  <p class="mb-0">© 2014-2018 Pingendo. All rights reserved</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <nav class="navbar bg-primary navbar-dark">
+    <div class="container">
+      <div class="container"><a class="navbar-brand" href="#">
+        </a>
+        <div class="row"><a class="navbar-brand" href="#">
+          </a>
+          <div class="text-l d-md-flex col-md-12 align-items-center justify-content-start"><a class="navbar-brand" href="#">
+            </a>
+            <ul class="nav d-inline-flex justify-content-start text-right">
+              <li class="nav-item"><a class="nav-link active" href="Turismo.html">Home</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="Quem samos.html">Quem samos</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="Tows.html">Tows</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="Pacote.html">Pacote</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="Contatos.html">Contatos</a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" style="">
+          <div class="carousel slide" data-ride="carousel" id="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="carousel-caption">
+                  <h5 class="m-0">Carousel</h5>
+                  <p>with controls</p>
+                </div>
+              </div>
+              <div class="carousel-item active">
+                <div class="carousel-caption">
+                  <h5 class="m-0">Carousel</h5>
+                  <p>b</p>
+                </div>
+              </div>
+              <div class="carousel-item"> <img class="d-block img-fluid w-100" src="https://static.pingendo.com/cover-moon.svg" height="250px">
+                <div class="carousel-caption">
+                  <h5 class="m-0">Carousel</h5>
+                  <p>with controls</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="embed-responsive embed-responsive-16by9">
+            <iframe src="https://player.vimeo.com/video/239823487?title=0&amp;byline=0&amp;portrait=0" allowfullscreen="" class="embed-responsive-item"></iframe>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/cover-bubble-light.svg"> </div>
+        <div class="col-md-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/cover-bubble-dark.svg"> </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3 col-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-1.svg"> </div>
+        <div class="col-md-3 col-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-2.svg"> </div>
+        <div class="col-md-3 col-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-3.svg"> </div>
+        <div class="col-md-3 col-6 p-3"> <img class="img-fluid d-block" src="https://static.pingendo.com/img-placeholder-4.svg"> </div>
+      </div>
+    </div>
+  </div>
+  <div class="py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="mx-auto text-center col-lg-6">
+          <h1 class="mb-3">I sink under</h1>
+          <p class="lead mb-4">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="p-0 order-2 order-md-1 col-lg-6"> <iframe width="100%" height="350" src="https://maps.google.com/maps?hl=en&amp;q=New%20York&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" scrolling="no" frameborder="0"></iframe> </div>
+        <div class="px-4 order-1 order-md-2 col-lg-6">
+          <h2 class="mb-4">A greater artist</h2>
+          <form>
+            <div class="form-group"> <input type="text" class="form-control" id="form44" placeholder="Name"> </div>
+            <div class="form-group"> <input type="email" class="form-control" id="form45" placeholder="Email"> </div>
+            <div class="form-group"> <textarea class="form-control" id="form46" rows="3" placeholder="Your message"></textarea> </div> <button type="submit" class="btn btn-primary">Send</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="">
+    <div class="container">
+      <div class="row">
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4">Pingendo</h2>
+          <p>A company for whatever you may need, from website prototyping to publishing</p>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4">Mapsite</h2>
+          <ul class="list-unstyled"> <a href="#" class="text-dark">Home</a> <br> <a href="#" class="text-dark">About us</a> <br> <a href="#" class="text-dark">Our services</a> <br> <a href="#" class="text-dark">Stories</a> </ul>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4">Contact</h2>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-phone"></i>+246 - 542 550 5462</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 text-muted fa-envelope-o"></i>info@pingendo.com</a> </p>
+          <p> <a href="#" class="text-dark">
+              <i class="fa d-inline mr-3 fa-map-marker text-muted"></i>365 Park Street, NY</a> </p>
+        </div>
+        <div class="p-4 col-md-3">
+          <h2 class="mb-4">Subscribe</h2>
+          <form>
+            <fieldset class="form-group"> <label for="exampleInputEmail1">Get our newsletter</label> <input type="email" class="form-control" placeholder="Enter email"> </fieldset> <button type="submit" class="btn btn-outline-dark">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12 mt-3">
+          <p class="text-center">© Copyright 2018 Pingendo - All rights reserved. </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 20px;right:20px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:220px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo Free&nbsp;&nbsp;<img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"></pingendo>
+</body>
+
+</html>
